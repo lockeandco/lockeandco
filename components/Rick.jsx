4dfb8f6e3b737ebe7d09df1367bb92413b2f2583
Austin Coose <austin.coose@radian.biz>
2018-09-03 11:18:21 -0600

    chevron size

--- components/Rick.jsx
+++ components/Rick.jsx
@@ -100,7 +100,7 @@ const CoFoundersRick = props => {
                     style={{ color: 'white' }}
                     onClick={() => Router.push('/co-founders')}
                   >
-                    <ChevronRight style={{ fontSize: 40 }} />
+                    <ChevronRight style={{ fontSize: 60 }} />
                   </IconButton>
                 </div>
                 <div
@@ -118,7 +118,7 @@ const CoFoundersRick = props => {
                     style={{ color: 'white' }}
                     onClick={() => Router.push('/co-founders/owen')}
                   >
-                    <ChevronLeft style={{ fontSize: 40 }} />
+                    <ChevronLeft style={{ fontSize: 60 }} />
                   </IconButton>
                 </div>
                 <div style={{ textAlign: 'center' }}>

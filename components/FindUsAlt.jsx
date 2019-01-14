6dbec0f8831466d5bcf7f3e644bd1fb6a3fd881d
Austin Coose <austin.coose@radian.biz>
2019-01-14 16:12:52 -0700

    Search, etc.

---
+++ components/FindUsAlt.jsx
@@ -0,0 +1,221 @@
+/*global google*/
+import React, { Component } from 'react'
+import { GMap } from 'primereact/gmap'
+import { Dialog } from 'primereact/dialog'
+import { InputText } from 'primereact/inputtext'
+import { Button } from 'primereact/button'
+import { Checkbox } from 'primereact/checkbox'
+import { Growl } from 'primereact/growl'
+import { GoogleApiWrapper } from 'google-maps-react'
+import { reject, map, compose, isEmpty, isNil, tap } from 'ramda'
+class GMapDemo extends Component {
+  constructor(props) {
+    super(props)
+    console.log('GMAPS DEMO', props)
+    this.state = {
+      dialogVisible: false,
+      markerTitle: '',
+      draggableMarker: false,
+      overlays: null,
+      selectedPosition: null,
+      //   zoom: 12,
+      itemInfo: {},
+    }
+
+    this.onMapClick = this.onMapClick.bind(this)
+    this.onOverlayClick = this.onOverlayClick.bind(this)
+    //  this.handleDragEnd = this.handleDragEnd.bind(this)
+    this.onMapReady = this.onMapReady.bind(this)
+    this.onHide = this.onHide.bind(this)
+    this.addMarker = this.addMarker.bind(this)
+    this.onZoomChanged = this.onZoomChanged.bind(this)
+    this.setInfoWindowContent = this.setInfoWindowContent.bind(this)
+  }
+
+  setInfoWindowContent(item) {
+    const { name, site, formatted_address } = item
+    console.log(item)
+    return `<div>
+    <h4>${name}</h4>
+    <h5>${formatted_address}<h5>
+    ${site ? `<a href=${site} target='_blank'>Homepage` : ''}
+    </div>`
+  }
+  componentDidUpdate(prev, next) {
+    // console.log(prev)
+    // console.log(next)
+    // console.log(this.props)
+    console.log(prev.position !== this.props.position)
+    const gm = this.gmap.getMap()
+
+    if (prev.position !== this.props.position) {
+      console.log('Setting Position')
+      gm.setCenter(this.props.position)
+    }
+    if (prev.zoom !== this.props.zoom) {
+      console.log('Setting Zoom')
+      gm.setZoom(this.props.zoom)
+    }
+    if (prev.zoom === 14 && prev.zoom > this.props.zoom) {
+      this.infoWindow && this.infoWindow.close()
+    }
+
+    if (
+      this.props.zoom === 14 &&
+      this.props.selectedItem.hasOwnProperty('location')
+    ) {
+      const item = this.props.selectedItem
+      prev.zoom === this.props.zoom || gm.setZoom(this.props.zoom)
+      //   const newMarker = new google.maps.Marker({
+      //     position: item.location,
+      //     title: item.name,
+      //     icon: {
+      //       url: '/static/plus.png',
+      //       // size: new google.maps.Size(20, 20),
+      //       scaledSize: new google.maps.Size(20, 20),
+      //     },
+      //   })
+      console.log(next.overlays)
+      if (Array.isArray(next.overlays)) {
+        const itemOverlay =
+          Array.isArray(next.overlays) &&
+          next.overlays.reduce((x, y) => (y.name === item.name ? y : x))
+
+        console.log(itemOverlay)
+        //   console.log(newMarker)
+        console.log(itemOverlay.getPosition())
+        //   console.log(newMarker.getPosition())
+        console.log('ITEM', item)
+        //   gm.setCenter(itemOverlay.getPosition())
+
+        this.infoWindow = this.infoWindow || new google.maps.InfoWindow()
+        this.infoWindow.setContent(this.setInfoWindowContent(item))
+        this.infoWindow.setPosition(item.location)
+        this.infoWindow.open(gm)
+        //gm.setCenter(item.location)
+
+        // gm.setCenter(itemOverlay.getPosition())
+        // gm.setZoom(this.props.zoom)
+      }
+    }
+  }
+  onMapClick(event) {
+    this.setState({
+      dialogVisible: true,
+    })
+    this.infoWindow && this.infoWindow.close()
+    this.props.setPositionAndZoom({
+      position: {
+        lat: 39.743642,
+        lng: -104.9854807,
+      },
+      zoom: 10,
+    })
+  }
+  onZoomChanged(d) {}
+  onOverlayClick(event) {
+    const { locs, setPositionAndZoom, setStore, expandList } = this.props
+    const title = event.overlay.getTitle()
+    const item = locs.reduce((x, y) => (y.name === title ? y : x))
+    // console.log(event.map)
+
+    if (title) {
+      this.infoWindow = this.infoWindow || new google.maps.InfoWindow()
+      this.infoWindow.setContent(this.setInfoWindowContent(item))
+      this.infoWindow.open(event.map, event.overlay)
+      setPositionAndZoom({
+        position: item.location,
+        zoom: 14,
+      })
+      console.log(item.city)
+      expandList(item.city || '')
+      setStore(item)
+    }
+  }
+
+  addMarker() {
+    let newMarker = new google.maps.Marker({
+      position: {
+        lat: this.state.selectedPosition.lat(),
+        lng: this.state.selectedPosition.lng(),
+      },
+      title: this.state.markerTitle,
+      draggable: this.state.draggableMarker,
+    })
+
+    this.setState({
+      overlays: [...this.state.overlays, newMarker],
+      dialogVisible: false,
+      draggableMarker: false,
+      markerTitle: '',
+    })
+  }
+
+  onMapReady(event) {
+    const {
+      locs,
+      position = {
+        lat: 39.743642,
+        lng: -104.9854807,
+      },
+      zoom = 10,
+    } = this.props
+
+    this.setState({
+      overlays: locs
+        ? [
+            ...compose(
+              map(l => {
+                const marker = new google.maps.Marker({
+                  position: l.location,
+                  title: l.name,
+                  icon: {
+                    url: '/static/plus.png',
+                    anchorPoint: new google.maps.Point(0, -200),
+                    scaledSize: new google.maps.Size(20, 20),
+                  },
+                })
+                return marker
+              }),
+              reject(isNil),
+              reject(isEmpty)
+            )(locs),
+          ]
+        : [],
+    })
+  }
+
+  onHide(event) {
+    // this.setState({ dialogVisible: false })
+  }
+
+  render() {
+    const { position, zoom } = this.props
+    const options = {
+      center: position,
+      zoom: zoom,
+    }
+    return (
+      <GMap
+        overlays={this.state.overlays}
+        ref={el => (this.gmap = el)}
+        options={options}
+        style={{
+          position: 'relative',
+          height: '100%',
+          width: '100%',
+          minHeight: '97vh',
+        }}
+        onMapReady={this.onMapReady}
+        onOverlayClick={this.onOverlayClick}
+        onOverlayDragEnd={this.handleDragEnd}
+        onMapClick={this.onMapClick}
+      />
+    )
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCAHEdspo9nrGHO9GqZxKwPXcjmOWr6mY4',
+  v: '3.33',
+})(GMapDemo)
---
+++ components/MaterialFormiklDownshift.jsx
@@ -0,0 +1,235 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import keycode from 'keycode'
+import Downshift from 'downshift'
+import IconButton from '@material-ui/core/Button'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import { withStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
+import Popper from '@material-ui/core/Popper'
+import Paper from '@material-ui/core/Paper'
+import MenuItem from '@material-ui/core/MenuItem'
+import match from 'autosuggest-highlight/match'
+import parse from 'autosuggest-highlight/parse'
+import Fuse from 'fuse.js'
+import { find, propEq, path } from 'ramda'
+import { ChevronDown, ChevronUp, Close } from 'mdi-material-ui'
+
+// TODO:  Provide mechanism for overriding options, styles, props
+
+const options = {
+  shouldSort: true,
+  threshold: 0.8,
+  location: 0,
+  distance: 300,
+  // tokenize: true,
+  maxPatternLength: 32,
+  minMatchCharLength: 1,
+  keys: ['label'],
+}
+
+function renderInput(inputProps) {
+  const { InputProps, fieldProps, classes, ref, ...other } = inputProps
+  return (
+    <TextField
+      {...fieldProps}
+      inputProps={{
+        style: {
+          background: 'rgba(226, 222, 213, 0.95)',
+          textTransform: 'uppercase',
+        },
+      }}
+      InputProps={{
+        inputRef: ref,
+        classes: {
+          root: classes.inputRoot,
+          input: classes.inputInput,
+        },
+        ...InputProps,
+      }}
+      {...other}
+    />
+  )
+}
+
+function renderSuggestion(
+  { suggestion, index, itemProps, menuClasses, highlightedIndex, selectedItem },
+  query
+) {
+  const isHighlighted = highlightedIndex === index
+  const isSelected = (selectedItem || '').indexOf(suggestion.label) > -1
+  const matches = match(suggestion.label, query)
+  const parts = parse(suggestion.label, matches)
+
+  return (
+    <MenuItem
+      {...itemProps}
+      className={menuClasses}
+      key={suggestion.label.concat(String(index))}
+      selected={isHighlighted}
+      component="div"
+      style={{
+        fontWeight: isSelected ? 500 : 400,
+      }}
+    >
+      <div>
+        {parts.map((part, index) => {
+          return part.highlight ? (
+            <span key={String(index)} style={{ fontWeight: 500 }}>
+              {part.text}
+            </span>
+          ) : (
+            <strong key={String(index)} style={{ fontWeight: 300 }}>
+              {part.text}
+            </strong>
+          )
+        })}
+      </div>
+    </MenuItem>
+  )
+}
+renderSuggestion.propTypes = {
+  highlightedIndex: PropTypes.number,
+  index: PropTypes.number,
+  itemProps: PropTypes.object,
+  selectedItem: PropTypes.string,
+  suggestion: PropTypes.shape({ label: PropTypes.string }).isRequired,
+}
+
+const styles = theme => ({
+  container: {
+    flexGrow: 1,
+    position: 'relative',
+  },
+  paper: {
+    position: 'absolute',
+    zIndex: 100,
+    marginTop: 0,
+    marginLeft: 5,
+    left: 0,
+    right: 0,
+    maxHeight: '50vh',
+    overflow: 'scroll',
+    background: 'rgba(226, 222, 213, 0.95)',
+  },
+  inputRoot: {
+    margin: 5,
+    // flexWrap: 'wrap',
+  },
+  inputInput: {
+    width: 'auto',
+    flexGrow: 1,
+    padding: 10,
+  },
+  iconButton: {
+    backgroundColor: 'rgba(226, 222, 213, 0.95)',
+    '&:hover': {
+      backgroundColor: 'rgba(226, 222, 213, 0.95)',
+    },
+  },
+})
+
+const IntegrationDownshift = props => {
+  const {
+    classes,
+    items,
+    handleChange,
+    fieldProps,
+    initialSelectedItem,
+    inputId,
+    disableUnderline,
+    menuClasses,
+  } = props
+  function getSuggestions(value) {
+    const inputValue = value.trim().toLowerCase()
+    const inputLength = inputValue.length
+    const fuse = new Fuse(items, options)
+    //const itemSorted = items.sort((x, y) => (x > y ? 1 : -1))
+    const fuseItems = fuse.search(inputValue)
+    return inputLength === 0 ? items : fuseItems
+  }
+  //console.log(items)
+  return (
+    <Downshift
+      //selectedItem={initialSelectedItem || ''}
+      onChange={handleChange}
+      inputId={inputId}
+    >
+      {({
+        getInputProps,
+        getItemProps,
+        getMenuProps,
+        getToggleButtonProps,
+        highlightedIndex,
+        inputValue,
+        isOpen,
+        selectedItem,
+        clearSelection,
+      }) => {
+        console.log('SelectedItem', selectedItem)
+        return (
+          <div className={classes.container}>
+            {renderInput({
+              fullWidth: true,
+              classes,
+              fieldProps,
+              InputProps: getInputProps({
+                disableUnderline: true,
+                endAdornment: (
+                  <InputAdornment variant="standard" position="end">
+                    {selectedItem ? (
+                      <IconButton
+                        aria-label="Clear Client List"
+                        onClick={clearSelection}
+                        className={classes.iconButton}
+                      >
+                        <Close />
+                      </IconButton>
+                    ) : (
+                      <IconButton
+                        className={classes.iconButton}
+                        aria-label="Toggle Client List"
+                        {...getToggleButtonProps()}
+                      >
+                        {isOpen ? <ChevronUp /> : <ChevronDown />}
+                      </IconButton>
+                    )}
+                  </InputAdornment>
+                ),
+              }),
+            })}
+            <div {...getMenuProps()}>
+              {isOpen ? (
+                <Paper className={classes.paper} square>
+                  {getSuggestions(inputValue).map((suggestion, index) =>
+                    renderSuggestion(
+                      {
+                        suggestion,
+                        index,
+                        itemProps: getItemProps({ item: suggestion.label }),
+                        menuClasses,
+                        highlightedIndex,
+                        selectedItem,
+                      },
+                      inputValue
+                    )
+                  )}
+                </Paper>
+              ) : null}
+            </div>
+          </div>
+        )
+      }}
+    </Downshift>
+  )
+}
+
+IntegrationDownshift.propTypes = {
+  classes: PropTypes.object.isRequired,
+  items: PropTypes.arrayOf(PropTypes.shape({ label: PropTypes.string }))
+    .isRequired,
+  handleChange: PropTypes.func.isRequired,
+  fieldProps: PropTypes.object,
+}
+
+export default withStyles(styles)(IntegrationDownshift)

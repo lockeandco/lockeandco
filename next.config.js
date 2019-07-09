f1f80017c8a2efee653e73c830828b73407a8d9b
Austin Coose <austin.coose@radian.biz>
2019-07-08 21:32:41 -0600

    Revert "netlfiy deploy"
    This reverts commit 454c2ccdc34e364a9c1d2261be3371734f70c739.

--- next.config.js
+++ next.config.js
@@ -1,44 +1,19 @@
-require('dotenv').config()
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-const path = require('path')
-const Dotenv = require('dotenv-webpack')
+
 const withMDX = require('@zeit/next-mdx')({
   extension: /.mdx?$/,
 })
 
-const {
-  PHASE_DEVELOPMENT_SERVER,
-  PHASE_PRODUCTION_BUILD,
-} = require('next/constants')
-
-module.exports = phase => {
-  // when started in development mode `next dev` or `npm run dev` regardless of the value of STAGING environmental variable
-  const isDev = phase === PHASE_DEVELOPMENT_SERVER
-  // when `next build` or `npm run build` is used
-  const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1'
-  // when `next build` or `npm run build` is used
-  const isStaging = PHASE_PRODUCTION_BUILD && process.env.STAGING === '1'
-
-  console.log(`isDev:${isDev}  isProd:${isProd}   isStaging:${isStaging}`)
-
-  // next.config.js object
-  return {
-    ...withMDX({
-      pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-      webpack: function(config, { isServer }) {
-        if (!isServer) {
-          config.plugins.push(
-            new CopyWebpackPlugin(['./static/favicon.ico'], {
-              debug: 'debug',
-            }),
-            new Dotenv({
-              path: path.join(__dirname, '.env'),
-              systemvars: true,
-            })
-          )
-        }
-        return config
-      },
-    }),
-  }
-}
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  webpack: function(config, { isServer }) {
+    if (!isServer) {
+      config.plugins.push(
+        new CopyWebpackPlugin(['./static/favicon.ico'], {
+          debug: 'debug',
+        })
+      )
+    }
+    return config
+  },
+})

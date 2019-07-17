776cd04b36814ce250f07674aec47cc24401ed4c
Austin Coose <austin.coose@radian.biz>
2019-07-17 00:15:01 -0600

    updates

--- amplify/backend/function/messagefromwebform/src/index.js
+++ amplify/backend/function/messagefromwebform/src/index.js
@@ -31,51 +31,6 @@ const SNS = new AWS.SNS({
   region: 'us-east-1',
 })
 
-// {
-//     "typeName": "Mutation",
-//     "fieldName": "createMessage",
-//     "arguments": {
-//         "input": {
-//             "itemId": "1",
-//             "itemTypeTarget": "Message",
-//             "Name": "Austin Coose",
-//             "Email": "austin.coose@gmail.com",
-//             "Type": "Casual Whiskey Drinker",
-//             "Body": "ksjsdjs"
-//         }
-//     },
-//     "identity": null,
-//     "source": null,
-//     "request": {
-//         "headers": {
-//             "x-forwarded-for": "174.29.202.93, 64.252.130.97",
-//             "accept-encoding": "gzip, deflate, br",
-//             "referer": "http://localhost:3000/contact-us",
-//             "cloudfront-viewer-country": "US",
-//             "cloudfront-is-tablet-viewer": "false",
-//             "via": "2.0 c9bc51c60922bd251aad1b28f0896cce.cloudfront.net (CloudFront)",
-//             "content-type": "application/json; charset=UTF-8",
-//             "x-api-key": "da2-hj5fpxjnd5ckfkluho5b27ralu",
-//             "origin": "http://localhost:3000",
-//             "cloudfront-forwarded-proto": "https",
-//             "x-amzn-trace-id": "Root=1-5d225882-d22049b811c2853ef41eaa96",
-//             "x-amz-cf-id": "lrRgx-pW8kfxzqAHlKZxEf_1gLtU5TCSiXOcpfccEnyf54a0cho3Jg==",
-//             "content-length": "350",
-//             "accept-language": "en-US,en;q=0.9",
-//             "x-forwarded-proto": "https",
-//             "host": "wxbfm6btzfejtld36nuf335j3i.appsync-api.us-west-2.amazonaws.com",
-//             "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
-//             "cloudfront-is-desktop-viewer": "true",
-//             "accept": "application/json, text/plain, */*",
-//             "cloudfront-is-mobile-viewer": "false",
-//             "x-forwarded-port": "443",
-//             "cloudfront-is-smarttv-viewer": "false"
-//         }
-//     },
-//     "prev": {
-//         "result": {}
-//     }
-// }
 exports.handler = async function (event, context ) { //eslint-disable-line
 
   const date = new Date(Date.now()).toString()
